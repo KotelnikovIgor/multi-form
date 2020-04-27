9884ca79da3124eb37dfb0828714e02f6bb9932e
Kotelnikov Igor <digikot1996@gmail.com>
2020-04-27 13:24:45 +0300

    Updates

---
+++ precache-manifest.3bb23832f99bd818fa3d286f951a17de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f649d2227853f9b88f6cff1e9a4f40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "def3a8084b355250aa40",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "af95a9115698a85d9b69",
+    "url": "/static/css/main.41144cb5.chunk.css"
+  },
+  {
+    "revision": "def3a8084b355250aa40",
+    "url": "/static/js/2.3a2656d2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3a2656d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af95a9115698a85d9b69",
+    "url": "/static/js/main.5742c51f.chunk.js"
+  },
+  {
+    "revision": "e447a45596fabe9d4632",
+    "url": "/static/js/runtime-main.a814e782.js"
+  }
+]);
